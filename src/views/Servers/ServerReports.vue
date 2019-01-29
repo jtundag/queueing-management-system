b9238d12c4a6263f9b095375be9ed4b275eec50d
Joshua <josh@baytech.ph>
2019-01-29 17:00:52 +0800

    get reports

--- src/views/Servers/ServerReports.vue
+++ src/views/Servers/ServerReports.vue
@@ -8,82 +8,72 @@
 import { Chart } from "highcharts-vue";
 
 export default {
-  components: {
-    highcharts: Chart
-  },
-  data() {
-    return {
-      chartOptions: {
-        chart: {
-          type: "spline"
-        },
-        title: {
-          text: "Monthly Report"
-        },
-        xAxis: {
-          categories: [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-            "Oct",
-            "Nov",
-            "Dec"
-          ]
-        },
-        yAxis: {
-          title: {
-            text: "Temperature"
-          },
-          labels: {
-            formatter: function() {
-              return this.value + "°";
-            }
-          }
-        },
-        tooltip: {
-          crosshairs: true,
-          shared: true
-        },
-        plotOptions: {
-          spline: {
-            marker: {
-              radius: 4,
-              lineColor: "#666666",
-              lineWidth: 1
-            }
-          }
-        },
-        series: [
-          {
-            name: "Tokyo",
-            marker: {
-              symbol: "square"
-            },
-            data: [
-              7.0,
-              6.9,
-              9.5,
-              14.5,
-              18.2,
-              21.5,
-              25.2,
-              26.5,
-              23.3,
-              18.3,
-              13.9,
-              9.6
-            ]
-          }
-        ]
-      }
-    };
-  }
+	components: {
+		highcharts: Chart
+	},
+	created(){
+
+	},
+	data() {
+		return {
+		chartOptions: {
+			chart: {
+				type: "spline"
+			},
+			title: {
+				text: "Monthly Report"
+			},
+			xAxis: {
+			categories: [
+				"Jan",
+				"Feb",
+				"Mar",
+				"Apr",
+				"May",
+				"Jun",
+				"Jul",
+				"Aug",
+				"Sep",
+				"Oct",
+				"Nov",
+				"Dec"
+			]
+			},
+			yAxis: {
+				title: {
+					text: "No. of Queues"
+				},
+				labels: {
+					formatter: function() {
+					return this.value + "°";
+					}
+				}
+			},
+			tooltip: {
+				crosshairs: true,
+				shared: true
+			},
+			plotOptions: {
+				spline: {
+					marker: {
+					radius: 4,
+					lineColor: "#666666",
+					lineWidth: 1
+					}
+				}
+			},
+			series: [
+				{
+					name: "Served",
+					marker: {
+					symbol: "square"
+					},
+					data: []
+				}
+			]
+		}
+		};
+	}
 };
 </script>
 
